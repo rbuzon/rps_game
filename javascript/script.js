e9281c5fc962387bcdf1649760a3805192d502f8
Rodney <buzonrodney@gmail.com>
2022-07-11 14:57:54 -0700

    added one round

--- javascript/script.js
+++ javascript/script.js
@@ -26,3 +26,48 @@ function capitalize(string) {
     return newstring.join("");
 }
 
+
+function playRound(playerchoice, computerchoice){
+
+    playerchoice = capitalize(playerchoice)
+
+    if (playerchoice == "Rock"){
+        switch(computerchoice) {
+            case "Rock":
+                return "It's a tie"
+            break;
+            case "Scissors":
+                return ("You Win! " + playerchoice + "beats " + computerchoice)
+            break;
+            case "Paper":
+                return ("You Lose! " + computerchoice + " beats " + playerchoice)
+        }
+    }
+    else if (playerchoice == "Paper"){
+        switch(computerchoice) {
+            case "Paper":
+                return "It's a tie"
+            break;
+            case "Rock":
+                return ("You Win! " + playerchoice + "beats " + computerchoice)
+            break;
+            case "Scissors":
+                return ("You Lose! " + computerchoice + " beats " + playerchoice)
+        }
+    }
+    else if (playerchoice == "Scissors"){
+        switch(computerchoice) {
+            case "Scissors":
+                return"It's a tie"
+            break;
+            case "Paper":
+                return ("You Win! " + playerchoice + "beats " + computerchoice)
+            break;
+            case "Rock":
+                return ("You Lose! " + computerchoice + " beats " + playerchoice)
+        }
+    }
+
+}   
+
+console.log(playRound(playerPlay(),computerPlay()))
